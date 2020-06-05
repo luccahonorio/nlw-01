55f43d099cf3d8d3818d03661ebd5c094014ad5d
Lucca Honorio <luccahmaraujo1@gmail.com>
2020-06-05 11:45:32 -0300

    App completo

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+
+
+
+//jsx sintaxe de xml dentro do Javascript
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
---
+++ web/src/components/Dropzone/index.tsx
@@ -0,0 +1,34 @@
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import './styles.css';
+import {FiUpload} from 'react-icons/fi'
+
+interface Props{
+    onFileUploaded: (file:File) =>void;
+}
+
+const Dropzone:React.FC<Props> = ({onFileUploaded}) => {
+
+    const  [selectedFileUrl,setselectedFileUrl] = useState('');
+    const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    const fileUrl = URL.createObjectURL(file);
+
+    setselectedFileUrl(fileUrl);
+    onFileUploaded(file);
+  }, [onFileUploaded])
+
+  const {getRootProps, getInputProps} = useDropzone(
+      {onDrop,
+        accept:'image/*'
+    })
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept='image/*' />
+
+      {selectedFileUrl ? <img src={selectedFileUrl} alt='point thumbnail'/> : (<p> <FiUpload/>Imagem do estabelicimento</p>)}  
+    </div>
+  )
+}
+export default Dropzone;
